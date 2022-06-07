0ebaefb9da9c210e4435dc1da0b752ff83ace4d9
ihssene drine <ihsene.drine@asm-dev.com>
2022-06-07 16:39:27 +0200

    add product Form validation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
   groceryList: any [] = [];
-  constructor() {}
+  productForm: FormGroup;
+  isSubmitted = false;
+  constructor(private fromBuilder: FormBuilder) {}
+  get errorControl() {
+    return this.productForm.controls;
+  }
+  ngOnInit(): void {
+    this.productForm = this.fromBuilder.group({
+      name : ['', [Validators.required]],
+      price : ['', [Validators.required]]
+    });
+  }
 
   addShopItem(item) {
     this.groceryList.push(item);
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.productForm.valid){
+      console.log('Please provide all the required values');
+      return false;
+    }
+    const product =  this.productForm.value;
+    this.groceryList.push(product);
   }
 }
